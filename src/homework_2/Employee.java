f3218db4e24080d16f94f33f3be48325a4b3e802
BohdanTaran <cronbodya@gmail.com>
2023-11-21 21:08:58 +0100

    added getters and setters to hw2

--- src/homework_2/Employee.java
+++ src/homework_2/Employee.java
@@ -20,4 +20,74 @@ public class Employee
         this.phoneNumber = phoneNumber;
         this.age = age;
     }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic()
+    {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic)
+    {
+        this.patronymic = patronymic;
+    }
+
+    public String getJobRole()
+    {
+        return jobRole;
+    }
+
+    public void setJobRole(String jobRole)
+    {
+        this.jobRole = jobRole;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
 }
